e7a493aa245e3b8b66e34d3341468327d0e1527a
Léon Hollender <hello@leonh.fr>
2022-09-17 01:21:11 +0200

    test: Run coverage

--- uci/uci_test.go
+++ uci/uci_test.go
@@ -1,8 +1,27 @@
 package uci
 
 import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
+
+func TestRun(t *testing.T) {
+	e := new(mockEngine)
+	e.On("Info").Return("NAME", "AUTHOR")
+	e.On("Options").Return([]Option{})
+	e.On("Quit")
+
+	r := strings.NewReader("uci\nfake command\nquit")
+	w := &strings.Builder{}
+
+	Run(e, r, w)
+
+	e.AssertExpectations(t)
+	assert.Equal(t, "id name NAME\nid author AUTHOR\nuciok\n", w.String())
+}
 
 // mockEngine is a mock that implements the Engine interface
 type mockEngine struct {
