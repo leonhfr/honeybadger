924d152668a2c5ccbcf873fcaba59d01e71869a3
Léon Hollender <hello@leonh.fr>
2022-10-15 20:37:06 +0200

    feat: Create position

--- chess/position.go
+++ chess/position.go
@@ -1,5 +1,10 @@
 package chess
 
+import (
+	"fmt"
+	"strings"
+)
+
 // CastlingRight represents the castling right of one combination of side and color.
 type CastlingRight uint8
 
@@ -13,3 +18,62 @@ const (
 	// CastleBlackQueen represents black's queen castle.
 	CastleBlackQueen
 )
+
+// Position represents the state of the game.
+type Position struct {
+	board           *board
+	turn            Color
+	castlingRights  CastlingRight
+	enPassantSquare Square
+	halfMoveClock   int
+	fullMoves       int
+}
+
+// FromFEN creates a Position from a FEN string.
+func FromFEN(fen string) (*Position, error) {
+	p := &Position{}
+	err := p.Unmarshal(fen)
+	return p, err
+}
+
+// Unmarshal assumes text is in Forsyth–Edwards Notation.
+func (p *Position) Unmarshal(fen string) error {
+	fields := strings.Fields(strings.TrimSpace(fen))
+	if len(fields) != 6 {
+		return fmt.Errorf("invalid fen (%s), must have 6 fields", fen)
+	}
+
+	var err error
+
+	p.board, err = fenBoard(fields[0])
+	if err != nil {
+		return err
+	}
+
+	p.turn, err = fenTurn(fields[1])
+	if err != nil {
+		return err
+	}
+
+	p.castlingRights, err = fenCastlingRights(fields[2])
+	if err != nil {
+		return err
+	}
+
+	p.enPassantSquare, err = fenEnPassantSquare(fields[3])
+	if err != nil {
+		return err
+	}
+
+	p.halfMoveClock, err = fenHalfMoveClock(fields[4])
+	if err != nil {
+		return err
+	}
+
+	p.fullMoves, err = fenFullMoves(fields[5])
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ chess/position_test.go
@@ -0,0 +1,25 @@
+package chess
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFromFEN(t *testing.T) {
+	tests := []struct {
+		args string
+		want error
+	}{
+		{"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", nil},
+		{"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPP/RNBQKBNR w KQkq - 0 1", errors.New("invalid fen rank field (PPPPPPP)")},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.args, func(t *testing.T) {
+			_, err := FromFEN(tt.args)
+			assert.Equal(t, tt.want, err)
+		})
+	}
+}
